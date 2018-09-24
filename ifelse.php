41c1a311b517f658a33ed2dfc663585238a91013
AS3-2-11 <AS3-2-11@uni.lan>
2018-09-24 12:50:10 +0200

    while

--- ifelse.php
+++ ifelse.php
@@ -3,8 +3,11 @@
 // https://www.w3schools.com/php/php_looping_for.asp
 // ejecutar codigo mas de una vez
 // print "a" 100 veces
-for ($taken =0; $taken++) {
-    echo "The number is: $x <br>";
+for ($taken =0; $taken <101; $taken++) {
+    echo "numero: $taken <br>";
+   echo "<br>"; 
+   echo "a";
+
 } 
-echo "a"
+
 
