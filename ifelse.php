f235f8ce4664ba3880d6c5c5cdf0dc19886ecddd
AS3-2-11 <AS3-2-11@uni.lan>
2018-09-24 12:40:24 +0200

    for

---
+++ ifelse.php
@@ -0,0 +1,10 @@
+<?php
+
+// https://www.w3schools.com/php/php_looping_for.asp
+// ejecutar codigo mas de una vez
+// print "a" 100 veces
+for ($taken =0; $taken++) {
+    echo "The number is: $x <br>";
+} 
+echo "a"
+
