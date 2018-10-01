b7567648e8bbefeceb3a8f362e512630dd6d0231
AS3-2-11 <AS3-2-11@uni.lan>
2018-10-01 13:05:35 +0200

    otratablaaa

--- calendario clase final.php
+++ calendario clase final.php
@@ -3,6 +3,8 @@
 // https://www.w3schools.com/php/php_arrays_multi.asp<?php
 // https://www.w3schools.com/php/php_arrays.asp
 // https://www.w3schools.com/php/php_arrays_multi.asp
+<table border="1">
+  <tr>
 $horario = array(
     array("WEB","WEB","SIST","SIST","RED","RED"),  // lunes 0
     array("WEB","RED","WEB","WEB","SIST","SIST"),  // martes 1
@@ -25,18 +27,30 @@ $horario2 = array(
     "Miercoles" => array("WEB","WEB","SIST","SIST","RED","RED"),  // miercoles 2
     "Jueves" => array("WEB","WEB","SIST","SIST","RED","RED"),  // jueves 3
     "Viernes" => array("WEB","WEB","SIST","SIST","RED","RED")  // viernes 4
-
+);
 $dia = date("N");
 $hora = date("G");
     
-    echo $horario[$dia-1][$hora-8]."<br>"
+    echo $horario[$dia-1][$hora-8]."<br>";
     // print horaio
-    foreach ($horario as $dia) {
-        foreach ($dia as $asig){
-            echo $asig. "<br>";
+   // foreach ($horario as $dia) {
+     //   foreach ($dia as $asig){
+            
+       //     echo $asig. "<br>";
+            
         }
     echo "------------------------<br>";
-    }
+    table width="100%"  border="1" cellpadding="0" cellspacing="0"> 
+<? for ($i=0; $i=5; $i++){ ?> 
+<tr> 
+<td>  echo $horario[$dia-1][$hora-8]."<br>";
+// print horaio
+foreach ($horario as $dia) {
+    foreach ($dia as $asig){
+        
+        echo $asig. "<br>";</td>  
+</tr><? } ?> 
+</table>
     
-);
-
+    }
+   
